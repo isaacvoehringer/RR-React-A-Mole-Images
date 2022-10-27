1d2282ff22325b69ef570886652e2bc7bc77ff60
isaac <voehringer.isaac@gmail.com>
2022-10-27 15:59:44 -0500

    whackamole

---
+++ react_a_mole/src/MoleContainer.js
@@ -0,0 +1,38 @@
+import {useState} from "react";
+import Mole from "./Mole";
+import EmptySlot from "./molehill";
+
+
+
+
+const MoleContainer = (props) => {
+    let [theMole, setTheMole] = useState(false)
+
+    const handleClick = (e) => {
+        props.setScore(props.score + 1)
+        setTheMole(false)
+    }
+
+    let displayMole = theMole ? <Mole setScore={props.setScore} toggle={setTheMole} handleClick={handleClick} /> : <EmptySlot toggle={setTheMole} />
+
+    return (
+        <div style={{'display': 'inline-block', 'width': '30vw'}}>
+            {displayMole}
+        </div>
+    )
+}
+
+export default MoleContainer
+// function Whack() {
+
+
+
+//     return 
+//             {moleFunc}
+//             {molehillFunc}
+// }
+
+// export default Whack
+
+
+
